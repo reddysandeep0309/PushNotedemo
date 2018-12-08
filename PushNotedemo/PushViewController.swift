82eaff031f4ef80b83eed093e682d059cfc312e2
Imran Mohammed <imran.mohammed3@ibm.com>
2018-12-08 20:47:07 +0530

    added new VC  "PushViewController.swift"

---
+++ PushNotedemo/PushViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PushViewController.swift
+//  PushNotedemo
+//
+//  Created by Imran Imu on 08/12/18.
+//  Copyright © 2018 Prolifics. All rights reserved.
+//
+
+import UIKit
+
+class PushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
